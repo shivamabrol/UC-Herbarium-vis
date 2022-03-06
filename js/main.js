32d6bdfa961c0c690e66ac282da5abb9a921bde1
Shivam Abrol <motoeshivam@gmail.com>
2022-03-06 07:43:04 -0600

    Dropdowns working

--- js/main.js
+++ js/main.js
@@ -28,39 +28,67 @@ d3.csv('data/data-sample2.csv')
   .catch(error => console.error(error));
 
 function configureDropDownLists(ddl1, ddl2) {
-  var years = [];
-  var startDaysofYear = [];
-  var classes = [];
-
-  for (let i = 1850; i <= 2000; i++) {
-    years.push(i);
-  }
 
   d3.csv('data/data-sample2.csv')
-    .then(data => {
-      classes = data.filter(d => d.class != 'null')
-    })
-    .catch(error => console.error(error));
+  .then(data => {
 
-  console.log(classes + 'is printed')
-  switch (ddl1.value) {
-    case 'year':
-      ddl2.options.length = 0;
-      for (i = 0; i < years.length; i++) {
-        createOption(ddl2, years[i], years[i]);
+    
+    year_null = data.filter(d => d.year != 'null')
+    class_null = data.filter(d => d.class != 'null')
+    startDay_null = data.filter(d => d.startDayOfYear != 'null')
+
+    var years = [];
+    var startDaysofYear = [];
+    var classes = [];
+    let minYear = d3.min(year_null, d => d.year),
+    maxYear = d3.max(year_null, d => d.year);
+
+    let firstStartDay = d3.min(startDay_null, d => d.startDayOfYear)
+    , lastStartDay = d3.max(startDay_null, d => d.startDayOfYear)
+
+    for (let i = minYear; i <= maxYear; i++) {
+      years.push(i);
+    }
+
+    for(let j = firstStartDay; j <= lastStartDay; j++) {
+      startDaysofYear.push(j);
+    }
+
+    data.forEach(d => {
+      // console.log(d);
+      if(!classes.includes(d.class)) {
+        classes.push(d.class)
+
       }
-      break;
-      break;
-    case 'startDayofYear':
-      ddl2.options.length = 0;
-      break;
-    case 'class':
-      ddl2.options.length = 0;
-      break;
-    default:
-      ddl2.options.length = 0;
-      break;
-  }
+    });
+
+    switch (ddl1.value) {
+      case 'year':
+        ddl2.options.length = 0;
+        for (i = 0; i < years.length; i++) {
+          createOption(ddl2, years[i], years[i]);
+        }
+        break;
+        break;
+      case 'startDayofYear':
+        ddl2.options.length = 0;
+        for (i = 0; i < startDaysofYear.length; i++) {
+          createOption(ddl2, startDaysofYear[i], startDaysofYear[i]);
+        }
+        break;
+      case 'class':
+        ddl2.options.length = 0;
+        for (i = 0; i < classes.length; i++) {
+          createOption(ddl2, classes[i], classes[i]);
+        }
+        break;
+      default:
+        ddl2.options.length = 0;
+        break;
+    }
+  })
+
+
 }
 
 
